f91c50262261fa759d097688e4bb1e53b1f99f85
KronChan <k@wuwii.com>
2018-04-16 23:51:31 +0800

    update

---
+++ wu-gateway/src/main/java/com/wuwii/WuGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.wuwii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author KronChan
+ * @version 1.0
+ * @since <pre>2018/4/16 22:54</pre>
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WuGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WuGatewayApplication.class, args);
+    }
+}
