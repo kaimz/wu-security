c0d1defe65a01f6e788613ff70f667ae858a452e
KronChan <k@wuwii.com>
2018-04-24 23:29:01 +0800

    test

---
+++ wu-module/wu-admin/src/test/com/wuwii/repositry/AdminUserRepositryTest.java
@@ -0,0 +1,24 @@
+package com.wuwii.repositry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author KronChan
+ * @version 1.0
+ * @since <pre>2018/4/24 23:25</pre>
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AdminUserRepositryTest {
+    @Autowired
+    private AdminUserRepositry adminUserRepositry;
+
+    @Test
+    public void testFindAll() {
+        System.out.println(adminUserRepositry.findAll());
+    }
+}
